90012ba1c81cc2ba757ceb48bd2194310055ebac
Olga Neniukova <Olga16.11.1989@yandex.ru>
2026-01-27 21:57:22 +0300

    Добавлена скобка для строки 12 в файле conftest.py

--- tests.py
+++ tests.py
@@ -1,24 +1,82 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
+#1
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
-        # добавляем две книги
+        collector = BooksCollector()        
         collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')        
         assert len(collector.get_books_rating()) == 2
+    
+#2  
+    def test_set_book_genre_valid_book_and_genre(self, setup_collector_with_book):
+        collector, book_name = setup_collector_with_book
+        genre_name = 'Детективы'
+        collector.set_book_genre(book_name, genre_name) 
+        assert collector.get_book_genre(book_name) == genre_name
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+#3   
+    def test_get_book_genre_by_the_name(self, setup_collector_with_book):
+        collector, book_name = setup_collector_with_book
+        genre_name = 'Фантастика'
+        collector.set_book_genre(book_name, genre_name) 
+        assert collector.get_book_genre(book_name) == genre_name
+
+#4
+    def test_get_book_with_specific_genre(self, setup_collector_with_book):
+        collector, book_name = setup_collector_with_book
+        genre_name = 'Детективы'
+        collector.set_book_genre(book_name, genre_name)
+        result = collector.get_books_with_specific_genre(genre_name)             
+        assert result == [book_name]
+
+#5   
+    def test_get_list_book_genre(self, setup_collector_with_book):
+        collector, book_name = setup_collector_with_book
+        genre_name = 'Фантастика'
+        collector.set_book_genre(book_name, genre_name)
+        result = collector.get_books_genre()
+        assert result == {book_name: genre_name}
+
+#6
+    def test_books_with_age_rating_not_in_children_list(self, setup_collector_with_book, genre_name, expected_result):
+        collector, book_name = setup_collector_with_book    
+        collector.set_book_genre(book_name, genre_name)    
+        children_books_list = collector.get_books_for_children()    
+        assert children_books_list == expected_result
+
+#7
+    def test_add_book_in_favorites_book(self, setup_collector_with_book):
+        collector, book_name = setup_collector_with_book    
+        collector.add_book_in_favorites(book_name)       
+        favorites_list = collector.get_list_of_favorites_books()
+        assert len(favorites_list) == 1
+        assert favorites_list == [book_name]
+
+#8
+    def test_book_is_already_in_favorites(self, setup_collector_with_favorited_book):
+        collector, book_name = setup_collector_with_favorited_book
+        assert book_name in collector.get_list_of_favorites_books()
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+#9
+    def test_delete_book_from_favorites(self, setup_collector_with_favorited_book):
+        collector, book_name = setup_collector_with_favorited_book       
+        assert book_name in collector.get_list_of_favorites_books()
+        initial_count = len(collector.get_list_of_favorites_books())        
+        collector.delete_book_from_favorites(book_name)       
+        favorites_list = collector.get_list_of_favorites_books()
+        assert book_name not in favorites_list
+        assert len(favorites_list) == initial_count - 1
+
+#10
+    def test_get_list_of_favorites_books(self, setup_collector_with_three_favorites):
+        collector, expected_books_list = setup_collector_with_three_favorites       
+        actual_favorites_list = collector.get_list_of_favorites_books()      
+        assert len(actual_favorites_list) == 3
+        assert actual_favorites_list == expected_books_list
+
+
+
+
+   

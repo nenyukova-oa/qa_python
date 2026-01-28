c1df313155eb6e247971cff3d21e6c18d9dd1613
Olga Neniukova <Olga16.11.1989@yandex.ru>
2026-01-28 13:39:04 +0300

    Все замечания устранены. Удалены лишние файлы и папки, в файлах conftest.py и README.md убрала букву f из нумерации, в файл conftest.py добавлена фикстура 1.1, тесты и фикстуры скорректированы с учетом 1.1. Из теста 6 убрала параметризацию, с параметризацией добавлена проверка 1.2. Из теста 9 удалены лишние проверки, теперь он атомарный. В README.md внесены правки в описание согласно внесенным изменениям

--- conftest.py
+++ conftest.py
@@ -1,32 +1,33 @@
 from main import BooksCollector
-
 import pytest
-#1f
+
+# 1.1
 @pytest.fixture (scope='function')
-def setup_collector_with_book():
-    collector = BooksCollector()
+def collector_instance():
+    return BooksCollector()
+
+# 1.2
+@pytest.fixture (scope='function')
+def setup_collector_with_book(collector_instance): 
     book_name = 'Пять поросят'
-    collector.add_new_book(book_name)
-    return collector, book_name
+    collector_instance.add_new_book(book_name)
+    return collector_instance, book_name
 
-#2f    
+# 2   
 @pytest.fixture(scope='function')
-def setup_collector_with_favorited_book():       
-    collector = BooksCollector()
+def setup_collector_with_favorited_book(collector_instance):      
     book_name = 'Война и мир'    
-    collector.add_new_book(book_name)
-    collector.add_book_in_favorites(book_name)     
-    return collector, book_name
+    collector_instance.add_new_book(book_name)
+    collector_instance.add_book_in_favorites(book_name)     
+    return collector_instance, book_name
 
-#3f
+# 3
 @pytest.fixture(scope='function')
-def setup_collector_with_three_favorites():       
-    collector = BooksCollector()
+def setup_collector_with_three_favorites(collector_instance): 
     book_names = ['Обломов', 'Зеленая миля', 'Сияние']        
         
     for name in book_names:
-        collector.add_new_book(name)
-        collector.add_book_in_favorites(name)         
+        collector_instance.add_new_book(name)
+        collector_instance.add_book_in_favorites(name)         
         
-    return collector, book_names
-
+    return collector_instance, book_names
--- test_books_collector.py
+++ test_books_collector.py
@@ -2,12 +2,25 @@ from main import BooksCollector
 import pytest
 
 class TestBooksCollector:
-#1
-    def test_add_new_book_add_two_books(self):
-        collector = BooksCollector()        
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')        
-        assert len(collector.get_books_genre()) == 2
+#1.1
+    def test_add_new_book_add_two_books(self, collector_instance):
+        collector_instance.add_new_book('Гордость и предубеждение и зомби')
+        collector_instance.add_new_book('Что делать, если ваш кот хочет вас убить')        
+        assert len(collector_instance.get_books_genre()) == 2
+
+#1.2
+    @pytest.mark.parametrize(
+        "book_name, expected_length",
+        [
+            ("", 0),
+            ("a" * 41, 0),            
+            ("b" * 50, 0)
+        ]
+    )
+    def test_add_new_book_invalid_length_not_added(self, collector_instance, book_name, expected_length):        
+        collector_instance.add_new_book(book_name)      
+        assert len(collector_instance.get_books_genre()) == expected_length
+        
     
 #2  
     def test_set_book_genre_valid_book_and_genre(self, setup_collector_with_book):
@@ -40,18 +53,14 @@ class TestBooksCollector:
         assert result == {book_name: genre_name}
 
 #6
-
-    @pytest.mark.parametrize("genre_name, expected_result",
-    [
-        ('Ужасы', []),        
-        ('Детективы', [])
-    ]
-    )
-    def test_books_with_age_rating_not_in_children_list(self, setup_collector_with_book, genre_name, expected_result):
-        collector, book_name = setup_collector_with_book    
-        collector.set_book_genre(book_name, genre_name)    
-        children_books_list = collector.get_books_for_children()    
+    def test_books_with_age_rating_not_in_children_list(self, setup_collector_with_book):
+        collector, book_name = setup_collector_with_book
+        genre_name = 'Ужасы'
+        expected_result = [] 
+        collector.set_book_genre(book_name, genre_name)
+        children_books_list = collector.get_books_for_children()
         assert children_books_list == expected_result
+
 
 #7
     def test_add_book_in_favorites_book(self, setup_collector_with_book):
@@ -69,13 +78,10 @@ class TestBooksCollector:
 
 #9
     def test_delete_book_from_favorites(self, setup_collector_with_favorited_book):
-        collector, book_name = setup_collector_with_favorited_book       
-        assert book_name in collector.get_list_of_favorites_books()
-        initial_count = len(collector.get_list_of_favorites_books())        
+        collector, book_name = setup_collector_with_favorited_book     
         collector.delete_book_from_favorites(book_name)       
         favorites_list = collector.get_list_of_favorites_books()
-        assert book_name not in favorites_list
-        assert len(favorites_list) == initial_count - 1
+        assert book_name not in favorites_list        
 
 #10
     def test_get_list_of_favorites_books(self, setup_collector_with_three_favorites):
